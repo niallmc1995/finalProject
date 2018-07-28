f17bccc6b8d7f86ccdc1e4bcec1dc9625c76328f
niallmc1995 <niallmcaffrey@gmail.com>
2018-07-28 12:41:17 +0100

    pulling in server side posts

--- backend/app.js
+++ backend/app.js
@@ -2,18 +2,23 @@ const express = require('express');
 
 const app = express();
 
-
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin","*");
+  res.setHeader("Access-Contorl-Allow-Header", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, Patch, DELETE, OPTIONS");
+  next();
+});
 
 app.use("/api/posts", (req, res, next) => {
   const posts = [
-    { id:'fadf12421l', title: 'first server-sdie post', content: 'This is coming from the server'},
-    { id:'ksajflaj132', title: 'second server-sdie post', content: 'This is coming from the server!'},
+    { id:'fadf12421l', title: 'first server-side post', content: 'This is coming from the server'},
+    { id:'ksajflaj132', title: 'second server-side post', content: 'This is coming from the server!'},
 
   ];
 res.status(200).json({
   message: 'Posts fetched succesfully!',
   posts: posts
-});
+  });
 });
 
 module.exports = app;
