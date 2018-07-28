80ad861943da6ebc193358e055898aaa830fdef8
niallmc1995 <niallmcaffrey@gmail.com>
2018-07-28 19:19:56 +0100

    pulling data/posts from the mongo server successfully

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+// var MongoClient = require('mongodb').MongoClient, Server = require('mongodb').Server;
 
+const Post = require("./models/post");
 const app = express();
+
+// MongoClient.connect("mongodb+srv://niall:Redmane_web_dev1995@cluster0-dpclk.mongodb.net/node-angular?retryWrites=true", { useNewUrlParser: true } )
+mongoose.connect("mongodb+srv://niallmc:Gizs3PbG8PuBazeo@cluster0-drbp9.mongodb.net/node-angular?retryWrites=true", { useNewUrlParser: true } )
+
+.then(() => {
+  console.log('Connected to database!')
+})
+
+.catch(() => {
+  console.log('Connection failed!');
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,30 +34,25 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
-  console.log(post);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save();
   res.status(201).json({
     message: 'Post added successfully'
   });
 });
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "fadf12421l",
-      title: "First server-side post",
-      content: "This is coming from the server"
-    },
-    {
-      id: "ksajflaj132",
-      title: "Second server-side post",
-      content: "This is coming from the server!"
-    }
-  ];
-  res.status(200).json({
-    message: "Posts fetched successfully!",
-    posts: posts
-  });
+  Post.find().then(documents => {
+    res.status(200).json({
+      message: "Posts fetched successfully!",
+      posts: documents
+    });
+  })
+
+
 });
 
 module.exports = app;
