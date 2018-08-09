a7cbd93752c12d63a17119a99771f39baaac072c
niallmc1995 <niallmcaffrey@gmail.com>
2018-08-09 20:17:01 +0100

    add authorization -- so that other users cannot edit other users content but can only see their content

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -28,7 +28,8 @@ export class PostsService {
                 title: post.title,
                 content: post.content,
                 id: post._id,
-                imagePath: post.imagePath
+                imagePath: post.imagePath,
+                creator: post.creator
               };
             }),
             maxPosts: postData.maxPosts
@@ -36,6 +37,7 @@ export class PostsService {
         })
       )
       .subscribe(transformedPostData => {
+       
         this.posts = transformedPostData.posts;
         this.postsUpdated.next({
           posts: [...this.posts],
@@ -54,6 +56,7 @@ export class PostsService {
       title: string;
       content: string;
       imagePath: string;
+      creator: string;
     }>("http://localhost:3000/api/posts/" + id);
   }
 
@@ -85,7 +88,8 @@ export class PostsService {
         id: id,
         title: title,
         content: content,
-        imagePath: image
+        imagePath: image,
+        creator: null
       };
     }
     this.http
